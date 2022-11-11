318c5debe5735d4c7f19f4dfbf9b046b4017ed64
unknown <saidi.firas@outlook.com>
2022-11-11 23:42:43 +0100

    fix2

--- src/sections/Header/Header .js
+++ src/sections/Header/Header .js
@@ -18,16 +18,16 @@ function Header () {
             <div className="flex-fill">
                 <ul className="nav navbar-nav d-flex justify-content-between mx-lg-auto">
                     <li className="nav-item">
-                        <Link className="nav-link" to="/">Home</Link>
+                        <Link className="nav-link" to={"/"}>Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
+                        <Link className="nav-link" to={"/about"}>About</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/shop">Shop</Link>
+                        <Link className="nav-link" to={"/shop"}>Shop</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/contact">Contact</Link>
+                        <Link className="nav-link" to={"/contact"}>Contact</Link>
                     </li>
                 </ul>
             </div>
