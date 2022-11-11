2952f78d590d94af2c583940a2505adab052c96d
unknown <saidi.firas@outlook.com>
2022-11-11 12:28:55 +0100

    fix router 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter } from 'react-router-dom'
 
 
 import App from './App';
@@ -9,9 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
       <App />
-    </HashRouter>
 
   </React.StrictMode>
 );
